198744d3c7389dcf86983fc78e0ceaf3d54cc90a
SimpleviewSoldier <codersgonnacode4life@gmail.com>
2017-03-19 11:35:40 -0700

    Update mine.js

--- mine.js
+++ mine.js
@@ -2,17 +2,13 @@ $(document).ready(function(){
 	
 		var target = $("div.outer");
 		
-	function setPosition(element, position, value){
-		$(element).css(position, value);	
-	}
-		
 		$(target).hover(function(){
 				var direction = $(this).entry({ e : event });
 				let overlay = $(this).find("div.inner");
 				
 				switch(direction){
 						case "up":
-						setPosition("top", "-100%");
+						$(overlay).css("top", "-100%").addClass("animate opac");
 						
 						break;
 					}

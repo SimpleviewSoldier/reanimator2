3213c8d9aa6642537915587d269e4da0d9532c00
SimpleviewSoldier <codersgonnacode4life@gmail.com>
2017-03-19 13:30:36 -0700

    Update mine.js

--- mine.js
+++ mine.js
@@ -1,34 +1,50 @@
 $(document).ready(function(){
 	
-		var target = $("div.outer");
-		
-		$(target).hover(function(){
-				var direction = $(this).entry({ e : event });
-				var overlay = $(this).find("div.inner");
-				
-				switch(direction){
-						case "up":
-						$(overlay).css("top", "-100%");
-						setTimeout(function(){
-							$(overlay).addClass("animate opac");
-						}, 10);
-						setTimeout(function(){
-							$(overlay).css("top", "0");
-						}, 10);
-						break;
-					}
-			}, function(){
-				var direction = $(this).entry({ e : event });
-				let overlay = $(this).find("div.inner");
-				
-				switch(direction){
-						case "up":
-						$(overlay).css("top", "-100%");
-						setTimeout(function(){
-							$(overlay).removeClass("animate opac");
-							$(overlay).css("top", "0");
-						}, 205);
-					}
-				
-				});
+	var target = $("div.outer");
+
+	$(target).hover(function(){
+		var direction = $(this).entry({ e : event });
+		var overlay = $(this).find("div.inner");
+
+		switch(direction){
+			case "up":
+			$(overlay).css("top", "-100%");
+			setTimeout(function(){
+				$(overlay).addClass("animate opac");
+			}, 10);
+			setTimeout(function(){
+				$(overlay).css("top", "0");
+			}, 10);
+			break;
+			case "down":
+			$(overlay).css("top", "100%");
+			setTimeout(function(){
+				$(overlay).addClass("animate opac");
+			}, 10);
+			setTimeout(function(){
+				$(overlay).css("top", "0");
+			}, 10);
+		}
+	}, function(){
+		var direction = $(this).entry({ e : event });
+		let overlay = $(this).find("div.inner");
+
+		switch(direction){
+			case "up":
+			$(overlay).css("top", "-100%");
+			setTimeout(function(){
+				$(overlay).removeClass("animate opac");
+				$(overlay).css("top", "0");
+			}, 205)
+			break;
+			case "down":
+			case "up":
+			$(overlay).css("top", "100%");
+			setTimeout(function(){
+				$(overlay).removeClass("animate opac");
+				$(overlay).css("top", "0");
+			}, 205)
+		}
+
 	});
+});

981a73a542b905e4a4b82d6222972cb7eb3ca6c4
SimpleviewSoldier <codersgonnacode4life@gmail.com>
2017-03-19 11:21:00 -0700

    Update mine.js

--- mine.js
+++ mine.js
@@ -12,9 +12,22 @@ $(document).ready(function(){
 				
 				switch(direction){
 						case "up":
+						console.log("Enter "+direction);
 						setPosition(overlay, "top", "-100%");
 						$(overlay).addClass("animate opac");
 						break;
 					}
-			});
+			}, function(){
+				var direction = $(this).entry({ e : event });
+				let overlay = $(this).find("div.inner");
+				
+				switch(direction){
+						case "up":
+						console.log("Exit "+direction);
+						setPosition(overlay, "top", "-100%");
+						$(overlay).addClass("animate opac");
+						break;
+					}
+				
+				});
 	});

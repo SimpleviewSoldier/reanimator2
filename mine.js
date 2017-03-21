1e70e634f8e87b569f95db61a10fcbc300389b0b
SimpleviewSoldier <codersgonnacode4life@gmail.com>
2017-03-20 20:54:35 -0700

    Update mine.js

--- mine.js
+++ mine.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 			setTimeout(function(){
 				requestAnimationFrame(function(){
 				$(panel).css({
-					"transform" : "",
+					"transform" : "translateY(0)",
 					"pointer-events" : "all"
 				});
 			});

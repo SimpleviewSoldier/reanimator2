c6e5ca3830512c00382610bc0736e3528981cc2f
SimpleviewSoldier <codersgonnacode4life@gmail.com>
2017-03-20 20:41:56 -0700

    Update mine.js

--- mine.js
+++ mine.js
@@ -1,166 +1,26 @@
 $(document).ready(function(){
-	
 	var target = $("div.outer");
 
-	$(target).hover(function(){
-		var direction = $(this).entry({ e : event });
-		var overlay = $(this).find("div.inner");
-
-		switch(direction){
+	$(target).hover(function(event){
+		var direction = $(this).entry({e : event});
+		var panel = $(this).find("div.inner");
+		switch (direction) {
 			case "up":
-			$(overlay).css("top", "-100%");
-			setTimeout(function(){
-				$(overlay).addClass("animate opac");
-			}, 5);
-			setTimeout(function(){
-				$(overlay).css("top", "0");
-			}, 5);
-			break;
-			case "down":
-			$(overlay).css("top", "100%");
-			setTimeout(function(){
-				$(overlay).addClass("animate opac");
-			}, 5);
-			setTimeout(function(){
-				$(overlay).css("top", "0");
-			}, 5);
-			break;
-			case "left":
-			$(overlay).css("left", "-100%");
-			setTimeout(function(){
-				$(overlay).addClass("animate opac");
-			}, 5);
-			setTimeout(function(){
-				$(overlay).css("left", "0");
-			}, 5);
-			break;
-			case "right":
-			$(overlay).css("left", "100%");
-			setTimeout(function(){
-				$(overlay).addClass("animate opac");
-			}, 5);
-			setTimeout(function(){
-				$(overlay).css("left", "0");
-			}, 5);
-			break;
+			$(panel).css({
+				"transform" : "translate"+axis+"("+value+")",
+				"will-change" : "transform"k
+				"pointer-events" : "none"
+			});
+			console.log("GO");
+			requestAnimationFrame(function(){
+				$(panel).addClass("animate");
+				$(panel).css({
+					"transform" : "",
+					"pointer-events" : "all"
+				});
+			});
 		}
-	}, function(){
-		var direction = $(this).entry({ e : event });
-		let overlay = $(this).find("div.inner");
-
-		switch(direction){
-			case "up":
-			$(overlay).css("top", "-100%");
-			setTimeout(function(){
-				$(overlay).removeClass("animate opac");
-				$(overlay).css("top", "0");
-			}, 205)
-			break;
-			case "down":
-			$(overlay).css("top", "100%");
-			setTimeout(function(){
-				$(overlay).removeClass("animate opac");
-				$(overlay).css("top", "0");
-			}, 205);
-			break;
-			case "left":
-			$(overlay).css("left", "-100%");
-			setTimeout(function(){
-				$(overlay).removeClass("animate opac");
-				$(overlay).css("left", "0");
-			}, 205);
-			break;
-			case "right":
-			$(overlay).css("left", "100%");
-			setTimeout(function(){
-				$(overlay).removeClass("animate opac");
-				$(overlay).css("left", "0");
-			}, 205);
-			break;
-		}
-
-	});
-});
-$(document).ready(function(){
-	
-	var target = $("div.outer");
-
-	$(target).hover(function(){
-		var direction = $(this).entry({ e : event });
-		var overlay = $(this).find("div.inner");
-
-		switch(direction){
-			case "up":
-			$(overlay).css("top", "-100%");
-			setTimeout(function(){
-				$(overlay).addClass("animate opac");
-			}, 5);
-			setTimeout(function(){
-				$(overlay).css("top", "0");
-			}, 5);
-			break;
-			case "down":
-			$(overlay).css("top", "100%");
-			setTimeout(function(){
-				$(overlay).addClass("animate opac");
-			}, 5);
-			setTimeout(function(){
-				$(overlay).css("top", "0");
-			}, 5);
-			break;
-			case "left":
-			$(overlay).css("left", "-100%");
-			setTimeout(function(){
-				$(overlay).addClass("animate opac");
-			}, 5);
-			setTimeout(function(){
-				$(overlay).css("left", "0");
-			}, 5);
-			break;
-			case "right":
-			$(overlay).css("left", "100%");
-			setTimeout(function(){
-				$(overlay).addClass("animate opac");
-			}, 5);
-			setTimeout(function(){
-				$(overlay).css("left", "0");
-			}, 5);
-			break;
-		}
-	}, function(){
-		var direction = $(this).entry({ e : event });
-		let overlay = $(this).find("div.inner");
-
-		switch(direction){
-			case "up":
-			$(overlay).css("top", "-100%");
-			setTimeout(function(){
-				$(overlay).removeClass("animate opac");
-				$(overlay).css("top", "0");
-			}, 205)
-			break;
-			case "down":
-			$(overlay).css("top", "100%");
-			setTimeout(function(){
-				$(overlay).removeClass("animate opac");
-				$(overlay).css("top", "0");
-			}, 205);
-			break;
-			case "left":
-			$(overlay).css("left", "-100%");
-			setTimeout(function(){
-				$(overlay).removeClass("animate opac");
-				$(overlay).css("left", "0");
-			}, 205);
-			break;
-			case "right":
-			$(overlay).css("left", "100%");
-			setTimeout(function(){
-				$(overlay).removeClass("animate opac");
-				$(overlay).css("left", "0");
-			}, 205);
-			break;
-		}
-
+	}, function(event){
+		var direction = $(this).entry({e : event});
 	});
 });
